e6452e52e37c950f01b199b2d01629b4436d7c56
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-02 21:06:31 +0530

    Widget application with accordion and wikipedia search api

---
+++ widget application/src/Components/Search.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+const Search = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      // console.log(data.query.search);
+      setResults(data.query.search);
+      // console.log(results);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <form className="ui form">
+        <div className="field">
+          <label htmlFor="">Enter search tem</label>
+          <input
+            value={term}
+            onChange={(e) => {
+              setTerm(e.target.value);
+              console.log(e.target.value);
+            }}
+            type="text"
+            name=""
+            id=""
+          />
+        </div>
+      </form>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
