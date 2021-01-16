3290282cbc663eee0907be45e347464c3414aa9e
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-16 17:48:20 +0530

    List building

--- blogs redux/src/index.js
+++ blogs redux/src/index.js
@@ -7,7 +7,7 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, appplyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
